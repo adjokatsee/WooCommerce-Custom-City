05b9e0e4b666848394dd5354d884b650c4a2ee50
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2016-08-20 13:48:19 +0800

    Update customcitywan.php

--- customcitywan.php
+++ customcitywan.php
@@ -1,15 +1,35 @@
 <?php
+
 function custom_override_default_address_fields($address_fields) {
-    //unset($address_fields['city']);
+    //var_dump($address_fields['state']);
     $address_fields['city'] = array(
         'type' => 'select',
-        'label' => 'Street District/Town',//Town / City
+        'label' => 'Street District/Town', //Town / City
         'required' => true,
         'class' => array('form-row-wide', 'address-field'),
         'autocomplete' => 'address-level2',
         'options' => array(
-            'option_1' => 'huhu grimace',
-            'gerimis' => 'gerimis'
+            'Taman Tunas Muda' => 'Taman Tunas Muda',
+            'SMK Sungai Ara' => 'SMK Sungai Ara',
+            'Persiaran Mahsuri' => 'Persiaran Mahsuri',
+            'Relau' => 'Relau',
+            'Gerbang Sungai Ara' => 'Gerbang Sungai Ara',
+            'Kampung Baru' => 'Kampung Baru',
+            'Kampung Tersusun' => 'Kampung Tersusun',
+            'Bayan Lepas' => 'Bayan Lepas',
+            'Bukit Gedung' => 'Bukit Gedung',
+            'Bayan Baru' => 'Bayan Baru',
+            'Sungai Tiram' => 'Sungai Tiram',
+            'Sungai Nibong Kecil' => 'Sungai Nibong Kecil',
+            'Teluk Kumbar' => 'Teluk Kumbar',
+            'Batu Maung' => 'Batu Maung',
+            'Pantai Jerejak' => 'Pantai Jerejak',
+            'Sungai Dua' => 'Sungai Dua',
+            'Batu Uban' => 'Batu Uban',
+            'Jelutung' => 'Jelutung',
+            'Sg. Pinang' => 'Sg. Pinang',
+            'Greenlane' => 'Greenlane',
+            'Batu Lancang' => 'Batu Lancang'
         )
     );
     return $address_fields;
